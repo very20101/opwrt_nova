70711e4e4a8c7cec20b66d2513b29a64c4f57763
welvery2010 <very20101@users.noreply.github.com>
2025-04-20 20:47:55 +0800

    Add files via upload

---
+++ scripts/functions.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+openwrt_repo="https://github.com/openwrt/openwrt.git"
+openwrt_pkg_repo="https://github.com/openwrt/packages.git"
+openwrt_luci_repo="https://github.com/openwrt/luci.git"
+immortalwrt_repo="https://github.com/immortalwrt/immortalwrt.git"
+immortalwrt_pkg_repo="https://github.com/immortalwrt/packages.git"
+immortalwrt_luci_repo="https://github.com/immortalwrt/luci.git"
+lede_repo="https://github.com/coolsnowwolf/lede.git"
+lede_pkg_repo="https://github.com/coolsnowwolf/packages.git"
+lede_luci_repo="https://github.com/coolsnowwolf/luci.git"
+istoreos_repo="https://github.com/istoreos/istoreos.git"
+lienol_pkg_repo="https://github.com/Lienol/openwrt-package"
+openwrt_apps_repo="https://github.com/ffuqiangg/openwrt_apps.git"
+passwall_pkg_repo="https://github.com/xiaorouji/openwrt-passwall-packages"
+passwall_luci_repo="https://github.com/xiaorouji/openwrt-passwall"
+dockerman_repo="https://github.com/lisaac/luci-app-dockerman"
+diskman_repo="https://github.com/lisaac/luci-app-diskman"
+docker_lib_repo="https://github.com/lisaac/luci-lib-docker"
+mosdns_repo="https://github.com/sbwml/luci-app-mosdns.git"
+openclash_repo="https://github.com/vernesong/OpenClash.git"
+node_prebuilt_repo="https://github.com/sbwml/feeds_packages_lang_node-prebuilt"
+nikki_repo="https://github.com/nikkinikki-org/OpenWrt-nikki.git"
+v2ray_geodata_repo="https://github.com/sbwml/v2ray-geodata"
+amlogic_repo="https://github.com/ophub/luci-app-amlogic.git"
+
+clone_repo () 
+{
+    repo_url=$1
+    branch_name=$2
+    target_dir=$3
+    git clone -b $branch_name --depth 1 $repo_url $target_dir
+}
+
+move_2_services () 
+{
+    local resource_file="$({ find | grep "\.lua\|\.htm\|\.json"; } 2>"/dev/null")"
+    for a in ${resource_file}; do
+        [ -n "$(grep "\"$1\"" "$a")" ] && sed -i "s,\"$1\",\"services\",g" "$a"
+        [ -n "$(grep "\"${1^^}\"" "$a")" ] && sed -i "s,\"${1^^}\",\"Services\",g" "$a"
+        [ -n "$(grep "\"${1^}\"" "$a")" ] && sed -i "s,\"${1^}\",\"Services\",g" "$a"
+        [ -n "$(grep "\[\[$1\]\]" "$a")" ] && sed -i "s,\[\[$1\]\],\[\[services\]\],g" "$a"
+        [ -n "$(grep "admin/$1" "$a")" ] && sed -i "s,admin/$1,admin/services,g" "$a"
+    done
+}
+
+docker_2_services () 
+{
+    local resource_file="$({ find | grep "\.lua\|\.htm"; } 2>"/dev/null")"
+    local dockerman_lua="$({ find | grep "dockerman\.lua"; } 2>"/dev/null")"
+    for a in ${resource_file}; do
+        [ -n "$(grep 'admin\",' "$a")" ] && sed -i "s|admin\",|& \"services\",|g" "$a"
+        [ -n "$(grep 'config\")' "$a")" ] && sed -i "s,config\"),overview\"),g" "$a"
+        [ -n "$(grep 'admin/' "$a")" ] && sed -i "s,admin/,&services/,g" "$a"
+        [ -n "$(grep 'admin\\/' "$a")" ] && sed -i "s,admin\\\/,&services\\\/,g" "$a"
+    done
+    sed -i 's,Docker,&Man,' ${dockerman_lua}
+}
---
+++ scripts/openwrt-24.10/02_prepare_package.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+. ../scripts/functions.sh
+
+### 基础部分 ###
+# 使用 O2 级别的优化
+sed -i 's/Os/O2/g' include/target.mk
+# 更新 Feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+# 移除 SNAPSHOT 标签
+sed -i 's,-SNAPSHOT,,g' include/version.mk
+sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
+
+### FIREWALL ###
+# custom nft command
+patch -p1 < ../patch/firewall/100-openwrt-firewall4-add-custom-nft-command-support.patch
+# patch LuCI 以支持自定义 nft 规则
+pushd feeds/luci
+patch -p1 < ../../../patch/firewall/04-luci-add-firewall4-nft-rules-file.patch
+popd
+
+### 替换准备 ###
+cp -rf ../openwrt-apps ./package/new
+rm -rf package/new/{luci-app-frpc,luci-app-frps,imm_pkg/frp}
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,microsocks,shadowsocks-libev,zerotier,daed,v2raya}
+rm -rf feeds/luci/applications/{luci-app-zerotier,luci-app-v2raya,luci-app-dockerman}
+rm -rf feeds/packages/utils/coremark
+
+### 获取额外的 LuCI 应用和依赖 ###
+# 预编译 node
+rm -rf ./feeds/packages/lang/node
+cp -rf ../node ./feeds/packages/lang/node
+# 更换 golang 版本
+rm -rf ./feeds/packages/lang/golang
+cp -rf ../openwrt_pkg_ma/lang/golang ./feeds/packages/lang/golang
+# mount cgroupv2
+pushd feeds/packages
+patch -p1 < ../../../patch/cgroupfs-mount/0001-fix-cgroupfs-mount.patch
+popd
+mkdir -p feeds/packages/utils/cgroupfs-mount/patches
+cp -rf ../patch/cgroupfs-mount/900-mount-cgroup-v2-hierarchy-to-sys-fs-cgroup-cgroup2.patch ./feeds/packages/utils/cgroupfs-mount/patches/
+cp -rf ../patch/cgroupfs-mount/901-fix-cgroupfs-umount.patch ./feeds/packages/utils/cgroupfs-mount/patches/
+cp -rf ../patch/cgroupfs-mount/902-mount-sys-fs-cgroup-systemd-for-docker-systemd-suppo.patch ./feeds/packages/utils/cgroupfs-mount/patches/
+# Docker 容器
+cp -rf ../dockerman/applications/luci-app-dockerman ./feeds/luci/applications/luci-app-dockerman
+sed -i '/auto_start/d' feeds/luci/applications/luci-app-dockerman/root/etc/uci-defaults/luci-app-dockerman
+pushd feeds/luci/applications/luci-app-dockerman
+docker_2_services
+popd
+# nlbw
+sed -i 's,services,network,g' package/feeds/luci/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
+# ttyd
+sed -i 's,services,system,g' package/feeds/luci/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
+# 晶晨宝盒
+cp -rf ../amlogic/luci-app-amlogic ./package/new/luci-app-amlogic
+
+#Vermagic
+latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/24.10/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
+wget https://downloads.openwrt.org/releases/${latest_version}/targets/armsr/armv8/packages/Packages.gz
+zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' > .vermagic
+sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+
+### 预配置一些插件 ###
+mkdir -p files
+cp -rf ../files/init/* files/
+mkdir -p files/etc/uci-defaults
+cp -f ../patch/default-settings/openwrt-24.10/zzz-default-settings ./files/etc/uci-defaults/
+
+find ./ -name *.orig | xargs rm -f
+find ./ -name *.rej | xargs rm -f
+
+exit 0
